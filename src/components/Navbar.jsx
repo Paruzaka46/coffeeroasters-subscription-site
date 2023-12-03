27e760fcb387698d04551a6c818706b5aa9dbb0d
Rayhan <rayhanfallah29@gmail.com>
2023-12-03 18:11:21 +0700

    Add Initial

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <div>
+                <img src="assets/shared/desktop/logo.svg"/>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
